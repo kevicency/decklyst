d1fd4875744d640a8a37b6436a6955ba4f15cbb8
Kevin Mees <kev.mees@gmail.com>
2022-07-30 17:49:31 +0200

    fix eslint

--- pages/api/deckcode/[deckcode].ts
+++ pages/api/deckcode/[deckcode].ts
@@ -1,23 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type {NextApiRequest, NextApiResponse} from 'next'
-import {CardData, cardData} from "../../../data/cards";
-import {parseDeckcode, validateDeckcode, DeckData} from "../../../lib/deckcode";
-
-const cardDataById = cardData.reduce((acc, cardData) => ({
-    ...acc,
-    [cardData.id]: cardData
-}), {} as Record<number, CardData>);
-
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { DeckData, parseDeckcode, validateDeckcode } from '../../../lib/deckcode'
 
 export default function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<DeckData | { error: string }>
+  req: NextApiRequest,
+  res: NextApiResponse<DeckData | { error: string }>,
 ) {
-    const deckcode = req.query.deckcode as string | undefined;
+  const deckcode = req.query.deckcode as string | undefined
+  const deckData = validateDeckcode(deckcode) ? parseDeckcode(deckcode) : null
 
-    if (validateDeckcode(deckcode)) {
-        res.status(200).json(parseDeckcode(deckcode))
-    } else {
-        res.status(400).json({error: 'Invalid deckcode provided'})
-    }
+  if (deckData) {
+    res.status(200).json(deckData)
+  } else {
+    res.status(400).json({ error: 'Invalid deckcode provided' })
+  }
 }
