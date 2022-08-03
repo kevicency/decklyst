8e23db60aef31e4047c5a3d2b86ec5343f08d7bb
Kevin Mees <kev.mees@gmail.com>
2022-08-03 22:55:15 +0200

    remove envs route

--- src/pages/api/envs.ts
+++
@@ -1,6 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-  res.status(200).json(process.env)
-}
