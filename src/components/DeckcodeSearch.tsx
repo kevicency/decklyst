5b5b25af667737a73a6ed88249bbfeb2d66480a8
Kevin Mees <kev.mees@gmail.com>
2022-08-11 01:25:17 +0200

    re-add search button

--- src/components/DeckcodeSearch.tsx
+++ src/components/DeckcodeSearch.tsx
@@ -3,6 +3,7 @@ import cx from 'classnames'
 import { useRouter } from 'next/router'
 import type { FC, KeyboardEventHandler } from 'react'
 import { useEffect, useRef, useState } from 'react'
+import { MdSearch } from 'react-icons/md'
 
 export const DeckcodeSearch: FC<{ big?: boolean }> = ({ big }) => {
   const inputElement = useRef<HTMLInputElement>(null)
@@ -39,10 +40,10 @@ export const DeckcodeSearch: FC<{ big?: boolean }> = ({ big }) => {
   }
 
   return (
-    <div className={cx('flex', big ? 'w-full' : 'w-96', big ? 'h-16' : 'h-auto')}>
+    <div className={cx('flex relative', big ? 'w-full' : 'w-96', big ? 'h-16' : 'h-auto')}>
       <input
         ref={inputElement}
-        className={cx('px-4 flex-1 w-64 bg-slate-900 border-2 border-slate-700', {
+        className={cx('pl-4 pr-14 flex-1 w-64 bg-slate-900 border-2 border-slate-700', {
           'border-red-500': touched && invalid,
           'text-xl': big,
         })}
@@ -52,15 +53,17 @@ export const DeckcodeSearch: FC<{ big?: boolean }> = ({ big }) => {
         onKeyDown={handleKeydown}
         onFocus={handleFocus}
       />
-      {/*<button*/}
-      {/*  disabled={invalid || navigating}*/}
-      {/*  className={cx('btn disabled:bg-slate-800 disabled:text-slate-600 px-2', {*/}
-      {/*    'px-6 text-xl': big,*/}
-      {/*  })}*/}
-      {/*  onClick={navigateToDeckcode}*/}
-      {/*>*/}
-      {/*  Go*/}
-      {/*</button>*/}
+      <button
+        disabled={invalid || navigating}
+        className={cx(
+          'btn disabled:bg-slate-900 disabled:text-slate-500',
+          'absolute right-[2px] top-[2px] h-[calc(100%-4px)]',
+          big ? 'pl-4 pr-3' : 'pl-2 pr-1.5',
+        )}
+        onClick={navigateToDeckcode}
+      >
+        <MdSearch size={big ? 32 : 24} />
+      </button>
     </div>
   )
 }
