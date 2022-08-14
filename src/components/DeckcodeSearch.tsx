8b5a50c4ecfcc3f2a0f1c54bcbd5798886194cb7
Kevin Mees <kev.mees@gmail.com>
2022-08-14 14:13:07 +0200

    ui fixes

--- src/components/DeckcodeSearch.tsx
+++ src/components/DeckcodeSearch.tsx
@@ -43,10 +43,14 @@ export const DeckcodeSearch: FC<{ big?: boolean }> = ({ big }) => {
     <div className={cx('flex relative', big ? 'w-full' : 'w-96', big ? 'h-16' : 'h-auto')}>
       <input
         ref={inputElement}
-        className={cx('pl-4 pr-14 flex-1 w-64 bg-slate-900 border-2 border-slate-700', {
-          'border-red-500': touched && invalid,
-          'text-xl': big,
-        })}
+        className={cx(
+          'pl-4 flex-1 w-64 bg-slate-900 border-2 border-slate-700',
+          big ? 'pl-4 pr-14' : 'pl-2 pr-11',
+          {
+            'border-red-500': touched && invalid,
+            'text-xl': big,
+          },
+        )}
         placeholder="Enter deckcode or share code"
         value={deckcode ?? ''}
         onChange={(ev) => setDeckcode(normalizeDeckcode(ev.target.value) ?? '')}
