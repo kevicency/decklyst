22240bcb27b632e581e09a6876a57f0b57cc08af
Kevin Mees <kev.mees@gmail.com>
2022-08-26 16:24:39 +0200

    enable cors for snapshot endpoint

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   output: process.env.NEXT_STANDALONE ? 'standalone' : undefined,
   reactStrictMode: true,
@@ -12,6 +12,23 @@ const nextConfig = {
       {
         source: '/i/:deckcode.png',
         destination: '/api/snapshot/:deckcode',
+      },
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/snapshot/:deckcode*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
       },
     ]
   },
