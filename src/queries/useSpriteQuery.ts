f18827e106b7487c686e57cf6bcd31c864575637
Kevin Mees <kev.mees@gmail.com>
2022-08-26 15:42:25 +0200

    cache sprite images forever

--- src/queries/useSpriteQuery.ts
+++ src/queries/useSpriteQuery.ts
@@ -7,31 +7,37 @@ export const useSpriteQuery = (cardId: number) => {
 
   const spriteImageUrl = `/assets/sprites/${cardId}.png`
 
-  const result = useQuery(['sprite', cardId], async () => {
-    const response = await fetch(spriteImageUrl)
-    const blob = await response.blob()
-    const blobUrl = await new Promise<string>((resolve) => {
-      const reader = new FileReader()
-      reader.onload = () => resolve(reader.result as string)
-      reader.readAsDataURL(blob)
-    })
-    const img = new Image()
-    const promise = new Promise<{ cardId: number; width: number; height: number; src: string }>(
-      (resolve) => {
-        img.onload = () => {
-          resolve({
-            cardId,
-            width: img.naturalWidth,
-            height: img.naturalHeight,
-            src: blobUrl,
-          })
-        }
-      },
-    )
-    img.src = blobUrl
+  const result = useQuery(
+    ['sprite', cardId],
+    async () => {
+      const response = await fetch(spriteImageUrl)
+      const blob = await response.blob()
+      const blobUrl = await new Promise<string>((resolve) => {
+        const reader = new FileReader()
+        reader.onload = () => resolve(reader.result as string)
+        reader.readAsDataURL(blob)
+      })
+      const img = new Image()
+      const promise = new Promise<{ cardId: number; width: number; height: number; src: string }>(
+        (resolve) => {
+          img.onload = () => {
+            resolve({
+              cardId,
+              width: img.naturalWidth,
+              height: img.naturalHeight,
+              src: blobUrl,
+            })
+          }
+        },
+      )
+      img.src = blobUrl
 
-    return promise
-  })
+      return promise
+    },
+    {
+      staleTime: Infinity,
+    },
+  )
 
   useEffect(() => {
     if (result.isSuccess) {
