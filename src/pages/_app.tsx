356355ecc8648ad84c17bf1d9b0ed852ef726a5c
Kevin Mees <kev.mees@gmail.com>
2022-08-29 00:14:33 +0200

    google site verification

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,8 +24,12 @@ function App({ Component, pageProps }: AppProps) {
           <Layout showSearch={router.route !== '/'}>
             <Head>
               <title>Decklyst</title>
-              <meta name="description" content="Share or create Duelyst 2 deck codes" />
+              <meta name="description" content="Share or create Duelyst 2 deckcodes" />
               <meta property="og:site_name" content="Decklyst" />
+              <meta
+                name="google-site-verification"
+                content="todbfz-oykJnt7ZTWmfVp8J6TyL1pnYoPMo6tZtkz-o"
+              />
               <link rel="icon" href="/public/favicon.ico" />
             </Head>
             <Component {...pageProps} />
