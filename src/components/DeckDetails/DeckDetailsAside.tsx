2b9ce742de53490c018260e2bcb92abcd0f83511
Kevin Mees <kev.mees@gmail.com>
2022-11-26 22:31:11 +0100

    ui: deck stat title

--- src/components/DeckDetails/DeckDetailsAside.tsx
+++ src/components/DeckDetails/DeckDetailsAside.tsx
@@ -43,9 +43,11 @@ export const DeckDetailsAside: FC = () => {
 
 export const DeckStat: FC<{ title: ReactNode; children: ReactNode }> = ({ children, title }) => {
   return (
-    <div className="mb-4 flex w-full flex-col gap-y-4 px-4">
-      <div className="text-semibold flex items-center justify-between font-semibold text-gray-300">
-        {title}
+    <div className="mb-4 flex w-full flex-col gap-y-3 px-4">
+      <div className="text-semibold flex items-center justify-between gap-x-2 text-lg font-semibold text-gray-300">
+        <div className="h-0.5 w-4 bg-gray-600" />
+        <span className="text-sm font-semibold uppercase text-gray-100">{title}</span>
+        <div className="h-0.5 flex-1 bg-gray-600" />
       </div>
       {children}
     </div>
