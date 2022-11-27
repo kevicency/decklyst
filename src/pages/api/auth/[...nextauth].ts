acb098a8e4e6837f844d3174b4f28b6123cfd9c0
Kevin Mees <kev.mees@gmail.com>
2022-11-27 16:54:15 +0100

    feat: update session age

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -23,6 +23,14 @@ export const authOptions: NextAuthOptions = {
       clientSecret: env.DISCORD_CLIENT_SECRET!,
     }),
   ],
+  session: {
+    // Seconds - How long until an idle session expires and is no longer valid.
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+    // Seconds - Throttle how frequently to write to database to extend a session.
+    // Use it to limit write operations. Set to 0 to always update the database.
+    // Note: This option is ignored if using JSON Web Tokens
+    updateAge: 24 * 60 * 60, // 24 hours
+  },
 }
 
 export default NextAuth(authOptions)
