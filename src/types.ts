eaaa4aa27bbcefc9c4cf742cd3acd983ba089332
Kevin Mees <kev.mees@gmail.com>
2022-11-29 22:59:26 +0100

    fix: types

--- src/types.ts
+++ src/types.ts
@@ -2,5 +2,5 @@ import type { RouterOutputs } from './utils/trpc'
 
 export type WithRequired<T, K extends keyof T> = T & { [P in K]-?: T[P] }
 
-export type UserProfile = RouterOutputs['userProfile']['get']
-export type DecklystWithAuthor = RouterOutputs['decklyst']['get']
+export type UserProfile = NonNullable<RouterOutputs['userProfile']['get']>
+export type DecklystWithAuthor = NonNullable<RouterOutputs['decklyst']['get']>
